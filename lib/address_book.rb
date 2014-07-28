95bc1e76af7fcd1dcd37bc4b797a1f6bd6aacb05
Jeff Stringer & Eric Johnson <student@epicodus.com>
2014-07-28 16:42:41 -0700

    add edit_email, edit_address methods

--- lib/address_book.rb
+++ lib/address_book.rb
@@ -1,11 +1,15 @@
 class Contact
 
-  attr_accessor :name, :phone_number
+  attr_accessor :name, :phone_number, :email, :address
 
-  def initialize(name, phone_num)
+  def initialize name, phone_num
     @name = name
     @phone_number = phone_num
   end
-
-
+  def edit_email input_email
+    @email = input_email
+  end
+  def edit_address input_address
+    @address = input_address
+  end
 end
