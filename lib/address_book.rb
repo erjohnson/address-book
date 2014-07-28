1858c9f53a33ee9ce5df33e3b51a4191a81a4777
Jeff Stringer & Eric Johnson <student@epicodus.com>
2014-07-28 16:25:04 -0700

    add initialize method

--- lib/address_book.rb
+++ lib/address_book.rb
@@ -1,7 +1,11 @@
 class Contact
 
-  def initialize(name)
+  attr_accessor :name, :phone_number
+
+  def initialize(name, phone_num)
     @name = name
+    @phone_number = phone_num
   end
+
 
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -3,9 +3,18 @@ require 'address_book'
 
 describe Contact do
   it 'is initialized by a name' do
-    test_contact = Contact.new 'Jerry Lewis'
+    test_contact = Contact.new 'Jerry Lewis', '212-555-5785'
     expect(test_contact).to be_an_instance_of Contact
   end
 
+  it 'contains the given name' do
+    test_contact = Contact.new 'Jerry Lewis', '212-555-5785'
+    expect(test_contact.name).to eq 'Jerry Lewis'
+  end
+
+  it 'contains the given phone number' do
+    test_contact = Contact.new 'Jerry Lewis', '212-555-5785'
+    expect(test_contact.phone_number).to eq '212-555-5785'
+  end
 
 end
